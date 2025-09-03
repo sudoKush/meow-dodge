6949a5e1ed89ab3e2608e9d48a3b454522916c97
sudoKush <404kushfound@gmail.com>
2025-09-03 23:58:34 +0530

    feat(main): add settings object as set and game window with game loop

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 import pygame
+import settings.settings as set
 
 pygame.init()
+
+screen = pygame.display.set_mode((set.screen_width, set.screen_height))
+pygame.display.set_caption("Meow Dodge")
+
+clock = pygame.time.Clock()
+
+playing = True
+
+# Colors
+gray = (50, 50, 50)
+
+while playing:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            playing = False
+
+    screen.fill(gray)
+
+    pygame.display.update()
+    clock.tick(set.fps)
+pygame.quit()
+
+
+
